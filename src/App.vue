f7afac114b13ce59ae25ac8c36af850ea6a9ec05
Kalys01 <kalysosmon96.g@gmail.com>
2021-04-26 22:37:32 +0600

    added HTML body

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,11 @@
   font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  // text-align: center;
   color: #2c3e50;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
 #nav {
